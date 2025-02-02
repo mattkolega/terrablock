e4315a39c06f7cfc7d143e7d330577f0319ef613
Matthew Kolega <12107095+mattkolega@users.noreply.github.com>
2025-02-02 20:05:13 +1100

    Run gameloop in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+#include "game.h"
+#include "utils/logger.h"
+
 int main() {
+    try {
+        Game game;
+        game.run();
+    } catch (const std::exception& e) {
+        Logger::err("{}", e.what());
+        return -1;
+    }
+
     return 0;
 }
